087a65944bf22cc85cebaa4bccbd804fe6e682d0
Iamruchitha1905 <sangeethasangeetha67177@gmail.com>
2026-02-27 09:16:52 +0530

    py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ig7(ww!hsmc5w^@r!1muiba2-iyeb)wqih%zj7n7^w%a^0d*+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,44 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+# ---------- Admin Site Titles ----------
+admin.site.site_header = "Polls Administration"
+admin.site.site_title = "Polls Admin Portal"
+admin.site.index_title = "Welcome to Polls Admin"
+
+
+# ---------- Inline Choices ----------
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+# ---------- Question Admin ----------
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_filter = ('pub_date',)
+    search_fields = ('question_text',)
+    date_hierarchy = 'pub_date'
+    inlines = [ChoiceInline]
+
+    # Attach custom admin CSS
+    class Media:
+        css = {
+            'all': ('admin/css/custom_admin.css',)
+        }
+
+
+# ---------- Choice Admin ----------
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'question', 'votes')
+    list_filter = ('question',)
+    search_fields = ('choice_text',)
+
+    # Attach custom admin CSS
+    class Media:
+        css = {
+            'all': ('admin/css/custom_admin.css',)
+        }
